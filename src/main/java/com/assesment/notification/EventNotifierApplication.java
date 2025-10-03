19cc53a5ce21132c66bf2f97a85e52601d048bc8
Kunal Kumar <kunal@Kunals-MacBook-Air.local>
2025-10-03 19:17:39 +0530

    spring boot inital project

---
+++ src/main/java/com/assesment/notification/EventNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.assesment.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventNotifierApplication.class, args);
+	}
+
+}
