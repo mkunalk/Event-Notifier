1a714c6ffc9fcc23227ad32c9fdc27548dd901f2
Kunal Kumar <kunal@Kunals-MacBook-Air.local>
2025-10-04 17:26:08 +0530

    Revert "email sms and push events configured"
    This reverts commit 01278577b3ffd1cb2df057801313b8ed9383c7c2.

--- src/main/java/com/assesment/notification/taskExcecutorConfig/PushNotificationProcessor.java
+++ src/main/java/com/assesment/notification/taskExcecutorConfig/PushNotificationProcessor.java
@@ -1,50 +1,19 @@
 package com.assesment.notification.taskExcecutorConfig;
 
-import com.assesment.notification.dataTo.CallBackResponse;
 import com.assesment.notification.dataTo.EventRequest;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Date;
 
 @Component
-@RequiredArgsConstructor
 public class PushNotificationProcessor {
-    private final FirebaseMessaging firebaseMessaging;
-    private final RestTemplate restTemplate;
 
     @Async("taskExecutor")
     public void processPushNotification(EventRequest pushNotificationDetails){
 
-        try{
-            Message message = Message
-                    .builder()
-                    .setToken(pushNotificationDetails.getDeviceId())
-                    .setNotification(Notification.builder()
-                            .setTitle("Sprih Assesment")
-                            .setBody(pushNotificationDetails.getMessage())
-                            .build())
-                    .build();
-            firebaseMessaging.send(message);
-            CallBackResponse response =
-                    new CallBackResponse(pushNotificationDetails.getEventId(), "COMPLETED",
-                            pushNotificationDetails.getEventType(), null, new Date());
-            HttpEntity<CallBackResponse> entity = new HttpEntity<>(response);
-            restTemplate.exchange(pushNotificationDetails.getCallbackUrl(), HttpMethod.POST, entity, void.class);
-        } catch (Exception e){
-            CallBackResponse response =
-                    new CallBackResponse(pushNotificationDetails.getEventId(), "FAILED",
-                            pushNotificationDetails.getEventType(), null, new Date());
-            HttpEntity<CallBackResponse> entity = new HttpEntity<>(response);
-            restTemplate.exchange(pushNotificationDetails.getCallbackUrl(), HttpMethod.POST, entity, void.class);
-        }
+        // we have to send notification here
+
+
+
     }
 
 }
--- src/test/java/com/assesment/notification/EventNotifierApplicationTests.java
+++ src/test/java/com/assesment/notification/EventNotifierApplicationTests.java
@@ -1,18 +1,13 @@
 package com.assesment.notification;
 
-import com.assesment.notification.controller.NotificationController;
-import lombok.RequiredArgsConstructor;
-import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+@SpringBootTest
+class EventNotifierApplicationTests {
 
-//@SpringBootTest
-//class EventNotifierApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+	@Test
+	void contextLoads() {
+	}
+
+}
